a5bc16f3874169fbc48b3c97bcb834fcc0cfa19e
Yihua Zhang <michaelzhang2@msn.com>
2016-10-23 15:03:24 -0400

    initialize iframe method

--- extension/javascript/content.js
+++ extension/javascript/content.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
+	// services
 	var Messenger = MessengerService("content");
 
-	function initializeIframes(){
-		var containerStyle = "position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0, 0, 0, 0.7); z-index: 2147483646; display:flex; justify-content:center; align-items:center;";
-		$container = $('<div/>', {id: "chrome-shell-top-container", style: containerStyle});
+	// base container
+	var containerStyle = "position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0, 0, 0, 0.7); z-index: 2147483646; display:flex; justify-content:center; align-items:center;";
+	var $container = $('<div/>', {id: "chrome-shell-top-container", style: containerStyle});
 
+	// iframes
+	var iframes = {};
+
+	function setIframe(id, styleString, target){
+		target = target || $container;
+
+		if(iframes[id]) {
+			return iframes[id];
+		} else {
+			// initialize new iframe
+			var src = chrome.extension.getURL("extension/iframes/html/" + id + ".html");
+			var iframe = $("<iframe/>", {id: "chrome-shell-iframe-" + id, src: src, scrolling: false, style: styleString});
+
+			// store iframe
+			iframes[id] = {
+				"id" : id
+				"iframe" : iframe,
+				"isLoaded" : false,
+			};
+
+			// append iframe
+			target.append(iframe);
+
+			return iframes[id];
+		}
+	};
+
+	// initialize
+	function initializeIframes(){
 		setTimeout(function(){
 			$('html').append($container);
 			$container.hide();
