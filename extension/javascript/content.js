968a218cf285c8bb77249527819367a0e71b5c2a
Yihua Zhang <michaelzhang2@msn.com>
2016-10-23 19:44:42 -0400

    adding animation to button in

--- extension/javascript/content.js
+++ extension/javascript/content.js
@@ -6,6 +6,8 @@ $(document).ready(function(){
 	var containerStyle = "position: fixed; top: 0; bottom: 0; left: 0; right: 0; background: rgba(0, 0, 0, 0.7); z-index: 2147483646; display:flex; justify-content:center; align-items:center;";
 	var $container = $('<div/>', {id: "chrome-shell-top-container", style: containerStyle});
 
+	var $baseButton, $homeButton, $messagesButton, $uploadButton, $screenshotButton;
+
 	// iframes
 	var iframes = {};
 
@@ -43,6 +45,8 @@ $(document).ready(function(){
 
 	initializeIframes();
 
+	// buttons
+
 	function generateButtonTemplate(buttonId, assetUrl){
 		var imageSrc = chrome.extension.getURL(assetUrl);
 		var buttonStyle = "background-image: url(" + imageSrc + "); background-position: 50% 50%; background-repeat: no-repeat;";
@@ -50,24 +54,49 @@ $(document).ready(function(){
 	};
 
 	function initializeButtons(){
-		setTimeout(function(){
-			var $baseButton = generateButtonTemplate("chrome-shell-base-button", "assets/images/boost-chrome-icon-48.png");
-			var $homeButton = generateButtonTemplate("chrome-shell-home-button", "assets/images/home-btn.png");
-			var $messagesButton = generateButtonTemplate("chrome-shell-messages-button", "assets/images/messages-btn.png");
-			var $uploadButton = generateButtonTemplate("chrome-shell-upload-button", "assets/images/upload-btn.png");
-			var $screenshotButton = generateButtonTemplate("chrome-shell-screenshot-button", "assets/images/screenshot-btn.png");
+		$baseButton = generateButtonTemplate("chrome-shell-base-button", "assets/images/boost-chrome-icon-48.png");
+		$homeButton = generateButtonTemplate("chrome-shell-home-button", "assets/images/home-btn.png");
+		$messagesButton = generateButtonTemplate("chrome-shell-messages-button", "assets/images/messages-btn.png");
+		$uploadButton = generateButtonTemplate("chrome-shell-upload-button", "assets/images/upload-btn.png");
+		$screenshotButton = generateButtonTemplate("chrome-shell-screenshot-button", "assets/images/screenshot-btn.png");
 
-			$('html').append($baseButton);
-			$('html').append($homeButton);
-			$('html').append($uploadButton);
-			$('html').append($messagesButton);
-			$('html').append($screenshotButton);
-		}, 1);
+		$('html').append($baseButton);
+		
+		$('html').append($homeButton);
+		$homeButton.hide();
+		$('html').append($uploadButton);
+		$messagesButton.hide();
+		$('html').append($messagesButton);
+		$uploadButton.hide();
+		$('html').append($screenshotButton);
+		$screenshotButton.hide();
 	};
 
 	initializeButtons();
 
+	function toggleButton(button){
+		var $button = $(button);
 
+		if($button.hasClass("fadeInRight")){
+			$button.removeClass("fadeInRight").addClass("fadeOutRight");
+			$button.one('oanimationend animationend', function(){
+				$button.hide();
+				$button.removeClass("fadeOutRight");
+			});
+		} else {
+			$button.show();
+			$button.addClass("animated fadeInRight");
+		}
+	};
+
+	function boostButtonClicked(){
+		toggleButton($homeButton);
+		toggleButton($messagesButton);
+		toggleButton($uploadButton);
+		toggleButton($screenshotButton);
+	};
+
+	$baseButton.click(boostButtonClicked);
 
 
 });
